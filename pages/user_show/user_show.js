91b0961d1c05308c2431e4fa3014ee9768e1c35e
Leo Chen <leoikaichen@gmail.com>
2018-08-03 09:55:49 +0800

    dryed map

--- pages/user_show/user_show.js
+++ pages/user_show/user_show.js
@@ -36,69 +36,45 @@ Page({
       method: 'GET',
       success: function (res) {
         console.log(28, res.data.user.longitude);
-        const user = res.data.user
-        const first_name = res.data.user.first_name;
-        const last_name = res.data.user.last_name;
-        const language = res.data.user.language;
-        const address = res.data.user.address;
-        const bio = res.data.user.bio;
-        const image_url = res.data.user.image_url;
-        const serviceid = res.data.user.services[0].id;
-        const price = res.data.user.price
+        const newUser = res.data.user
+        // const first_name = res.data.user.first_name;
+        // const last_name = res.data.user.last_name;
+        // const language = res.data.user.language;
+        // const address = res.data.user.address;
+        // const bio = res.data.user.bio;
+        // const image_url = res.data.user.image_url;
+        // const serviceid = res.data.user.services[0].id;
+        // const price = res.data.user.price
 
         page.setData({
-          id: options.id,
-          first_name: first_name,
-          last_name: last_name,
-          address: address,
-          bio: bio,
-          image_url: image_url,
-          price: price,
-          user: user,
-          serviceid: serviceid
+          // id: options.id,
+          // first_name: first_name,
+          // last_name: last_name,
+          // address: address,
+          // bio: bio,
+          // image_url: image_url,
+          // price: price,
+          'user': newUser,
+          // serviceid: serviceid
+          'map.hasMarkers': true,
+          'markers': [{
+            iconPath: "/icons/shared_icons/marker.png",
+            latitude: newUser.latitude,
+            longitude: newUser.longitude,
+            width: 50,
+            height: 50,
+          }],
+          'circles': [{
+            latitude: newUser.latitude,
+            longitude: newUser.longitude,
+            radius: 300,
+            fillColor: "#00000015",
+            color: "#74CFCC",
+            strokeWidth: 2
+          }]
         })
       }
-    }),
-      wx.getStorage({
-        key: 'user_id',
-        success: function (res) {
-          // const userId = res.data
-          wx.request({
-           // url: `http://localhost:3000/api/v1/users/${sitterId}`,         
-            url: `https://dog-sitter-woof.herokuapp.com/api/v1/users/${sitterId}`,
-            method: 'GET',
-            success(res) {
-              console.log("res", res)
-              const user = res.data.user;
-              const longitude = res.data.user.longitude;
-              const latitude = res.data.user.latitude;
-              console.log(55,typeof longitude);
-              console.log(56, typeof user);
-              page.setData({
-                'user': user,
-                'user.longitude': longitude,
-                'user.latitude': latitude,
-                'map.hasMarkers': true,
-               'markers': [{
-                  iconPath: "/icons/shared_icons/marker.png",
-                  latitude: latitude,
-                  longitude: longitude,
-                  width: 50,
-                  height: 50,
-                }],
-                'circles': [{
-                  latitude: latitude,
-                  longitude: longitude,
-                  radius: 300,
-                  fillColor: "#00000015",
-                  color: "#74CFCC",
-                  strokeWidth: 2
-                }]
-                });
-            }
-          });
-        }
-    });
+    })
   },
 
   onClick: function (e) {
